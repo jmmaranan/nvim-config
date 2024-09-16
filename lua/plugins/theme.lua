e105c4af34fb713b0faa20bda62bc2f78375b160
Jose Maranan <jmmaranan@gmail.com>
2024-09-16 14:44:13 -0400

    Update theme to catpuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,46 @@
-return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+-- return {
+--   "maxmx03/solarized.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function(_, opts)
+--     vim.o.termguicolors = true
+--     vim.o.background = "light"
+--     require("solarized").setup(opts)
+--     vim.cmd.colorscheme("solarized")
+--   end,
+-- }
+
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
     },
   },
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "light"
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin-latte")
+  end,
 }
