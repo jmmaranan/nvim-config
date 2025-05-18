a1fb6f9491f295cee2c3d039304bc3ae7862603e
Jose Maranan <jmmaranan@gmail.com>
2025-05-18 17:03:43 -0400

    Mason updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,6 @@
 return {
-  "williamboman/mason.nvim",
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   opts = {
     ensure_installed = {
       "bash-language-server",
