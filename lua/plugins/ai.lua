0d30d14b3e019a20b2df722251f36775d24a84b8
Jose Maranan <josemarie.maranan@delta.com>
2025-09-03 19:18:45 -0400

    Use AmazonQ completion for work

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,15 @@
+return {
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   opts = {
+  --     model = "claude-sonnet-4",
+  --   },
+  -- },
+  {
+    name = "amazonq",
+    url = "https://github.com/awslabs/amazonq.nvim.git",
+    opts = {
+      ssoStartUrl = "https://deltaic.awsapps.com/start",
+    },
+  },
+}
--- lua/plugins/copilotchat.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = {
-      model = "claude-sonnet-4",
-    },
-  },
-}
